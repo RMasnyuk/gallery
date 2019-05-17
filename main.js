fa6f73972dccf3cbfba3c068aa26b0ec3c8f3844
Rostislav Masniuk <rostislav.masnyuk.98@gmail.com>
2019-05-17 16:09:31 +0300

    solution

---
+++ main.js
@@ -0,0 +1,16 @@
+let thumbs = document.querySelector('#thumbs');
+let bigImage = document.querySelector('#largeImg');
+
+thumbs.onclick = function (event) {
+    event.preventDefault();
+    let target = event.target;
+    if (target.nodeName === 'DIV') {
+        return;
+    } else if (target.nodeName === 'IMG') {
+        bigImage.alt = target.parentNode.title;
+        bigImage.src = target.parentElement.href;
+        return;
+    }
+    bigImage.alt = target.title;
+    bigImage.src = target.href;
+}
