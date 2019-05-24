53c159f0af307635d3be9fc322855b812c06c3f9
Rostislav Masniuk <rostislav.masnyuk.98@gmail.com>
2019-05-24 12:52:45 +0300

    solution

--- main.js
+++ main.js
@@ -4,13 +4,9 @@ let bigImage = document.querySelector('#largeImg');
 thumbs.onclick = function (event) {
     event.preventDefault();
     let target = event.target;
-    if (target.nodeName === 'DIV') {
-        return;
-    } else if (target.nodeName === 'IMG') {
-        bigImage.alt = target.parentNode.title;
-        bigImage.src = target.parentElement.href;
+    if (target.tagName !== 'IMG') {
         return;
     }
-    bigImage.alt = target.title;
-    bigImage.src = target.href;
+    bigImage.alt = target.parentNode.title;
+    bigImage.src = target.parentNode.href;
 }
