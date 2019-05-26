614524ab98bb64503dce85a004eecac3256f70c0
Rostislav Masniuk <rostislav.masnyuk.98@gmail.com>
2019-05-26 14:37:16 +0300

    solution

--- main.js
+++ main.js
@@ -4,15 +4,7 @@ let bigImage = document.querySelector('#largeImg');
 thumbs.onclick = function (event) {
     event.preventDefault();
     let target = event.target;
-<<<<<<< HEAD
-    if (target.nodeName === 'DIV') {
-        return;
-    } else if (target.nodeName === 'IMG') {
-        bigImage.alt = target.parentElement.title;
-        bigImage.src = target.parentElement.href;
-=======
     if (target.tagName !== 'IMG') {
->>>>>>> develop
         return;
     }
     bigImage.alt = target.parentNode.title;
